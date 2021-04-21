8566747a96510ec62d38d02480483558c9d983c9
Matteo <877485@stud.unive.it>
2021-04-21 15:43:01 +0200

    renamed main in runner

--- runner.py
+++ runner.py
@@ -1,8 +1,4 @@
-"""
-
-
-"""
-from app import create_app
+from sources import create_app
 
 
 app = create_app()
@@ -11,5 +7,5 @@ app = create_app()
 # la seguente parte di programma viene eseguita solo se il nome del file a runtime è __main__
 if __name__ == "__main__":
     # debug=True fornisce messaggi di output del server più dettagliati
-    # quando l'app viene confezionata per l'uso bisogna togliere la modalità debug
+    # FIXME: quando la webapp viene confezionata per l'uso bisogna togliere la modalità debug
     app.run(debug=True)
