5ccf80183dde019a73e753d3f3c5c1d69b2bdb78
Matteo <877485@stud.unive.it>
2021-04-26 00:43:10 +0200

    updated error handling

--- sources/main/errors.py
+++ sources/main/errors.py
@@ -6,14 +6,14 @@ from . import main
 
 @main.app_errorhandler(403)
 def forbidden(e):
-    return render_template("403.html"), 403
+    return render_template("error.html", number=403, message='Forbidden'), 403
 
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 400
+    return render_template("error.html", number=404, message='Not Found'), 400
 
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
-    return render_template("500.html"), 500
+    return render_template("error.html", number=500, message='Internal Server Error'), 500
