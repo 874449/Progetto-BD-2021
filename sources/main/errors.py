d46a702edb9208292360b8f0c4df59add0078783
Matteo <877485@stud.unive.it>
2021-04-26 23:25:15 +0200

    minor changes

--- sources/main/errors.py
+++ sources/main/errors.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from . import main
 
-# TODO: aggiungere altre tipologie di errori e sistemare le pagine html
+# TODO: aggiungere altre tipologie di errori
 
 
 @main.app_errorhandler(403)
@@ -17,3 +17,4 @@ def page_not_found(e):
 @main.app_errorhandler(500)
 def internal_server_error(e):
     return render_template("error.html", number=500, message='Internal Server Error'), 500
+
