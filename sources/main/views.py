a09e04f26157a959229b6ed95d667ec9b9ec9625
Matteo <877485@stud.unive.it>
2021-04-27 21:31:04 +0200

    started home logged page

--- sources/main/views.py
+++ sources/main/views.py
@@ -6,3 +6,10 @@ from . import main
 def index():
     return render_template("index.html")
 
+
+@main.route('/<str:id>')
+def main_page(id):
+    if id == session['email']:
+        return render_template("home.html")
+    else:
+        return render_template("error.html", number=403, message='Something went wrong')
