5d00f0313eb4e0accab292ccb5b4d9a2c304a403
Matteo <877485@stud.unive.it>
2021-05-05 09:58:40 +0200

    modified for flask_login support

--- sources/auth/views.py
+++ sources/auth/views.py
@@ -1,22 +1,29 @@
 """
 In questo modulo si trovano tutte le route riguardandi l'autenticazione nel sito
 """
-from flask import render_template, request, flash, redirect, session
-# from flask_login import login_user, logout_user, login_required, current_user
+from flask import render_template, request, flash, redirect, session, url_for
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
+from ..models import User
+from . forms import LoginForm
 from .. import db
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+    return render_template("login.html", form=form)
 
 
 @auth.route('/logout')
-# @login_required
+@login_required
 def logout():
-    # logout_user()
-    session["email"] = None
+    logout_user()
     flash('You have been logged out.')
     return redirect('/')
 
