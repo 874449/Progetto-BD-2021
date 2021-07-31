055486930bd6049c305cbad6e4fd7e84774e02c8
Matteo <877485@stud.unive.it>
2021-07-31 12:55:33 +0200

    added heroku deploy files

--- config.py
+++ config.py
@@ -22,14 +22,13 @@ class DevelopmentConfig(Config):
 
 
 class ProductionConfig(Config):
-    pass
-    # TODO
-    # SQLALCHEMY_DATABASE_URI = heroku
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'postgres://aavtmophgawudv:83aa3c2bbd95652a10eda1087689cba044f599829a5aff824037eda7b11ed141@ec2-176-34-105-15.eu-west-1.compute.amazonaws.com:5432/d8tdh6u9hcd2tf'
 
 
 config = {
     'development': DevelopmentConfig,
     'production': ProductionConfig,
 
-    'default': DevelopmentConfig
+    'default': ProductionConfig
 }
