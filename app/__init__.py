36d8fa64bee99942f3c17ddf771db5fd3c97aa52
Matteo <877485@stud.unive.it>
2021-07-31 17:19:22 +0200

    minor changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
-"""
-il file __init__.py serve per python a trovare i package di cui fare import.
-Avendo fatto una cartella separata il file runner.py non può fare import create_app senza questo nome del file.
-"""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
-from . commands import create_tables
-# from flask_bootstrap import Bootstrap
+
 from config import config
+from .commands import create_tables
+from .main import main
+from .auth import auth
+from .quiz import quiz
+
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -27,13 +27,8 @@ def create_app(config_name):
     db.init_app(app)
 
     # nelle seguenti linee stiamo creando le blueprint per dividere il progetto in più file
-    from .main import main
     app.register_blueprint(main, url_prefix='/')
-
-    from .auth import auth
     app.register_blueprint(auth, url_prefix='/auth')
-
-    from .quiz import quiz
     app.register_blueprint(quiz, url_prefix='/quiz')
 
     app.cli.add_command(create_tables)
