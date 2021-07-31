5179f25bd62e6efa204dc57f7b10f1b3a44ee547
Matteo <877485@stud.unive.it>
2021-07-31 12:55:38 +0200

    New view

--- sources/main/views.py
+++ sources/main/views.py
@@ -1,22 +1,23 @@
 from flask import render_template, request, session
-from flask_login import login_required
+from flask_login import login_required, current_user
+from .. models import *
+from .. import db
 from . import main
+import datetime
 
 
 @main.route('/')
 def index():
+    if current_user.is_authenticated:
+        return render_template('index.html')
     return render_template("index.html")
 
 
-@main.route('/secret')
-@login_required
-def secret():
-    return '<h1>Shhhh!</1><p>It\'s a secret</p>'
-
-
-@main.route('/dashboard')
+@main.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
+    if request.method == 'POST':
+        nuovo = Questionario('Nuovo', datetime.datetime)
     return render_template('dashboard.html')
 
 
