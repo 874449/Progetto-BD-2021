739e96363ff2ad17b3f9d248b795e181a381d99e
Matteo <877485@stud.unive.it>
2021-08-03 10:20:15 +0200

    remode debug option from runner
    The debug mode now is handled only by environment variables in config.py file

--- runner.py
+++ runner.py
@@ -9,6 +9,4 @@ migrate = Migrate(app, db)
 
 # la seguente parte di programma viene eseguita solo se il nome del file a runtime è __main__
 if __name__ == "__main__":
-    # debug=True fornisce messaggi di output del server più dettagliati
-    # FIXME: quando la webapp viene confezionata per l'uso bisogna togliere la modalità debug
-    app.run(debug=True)
+    app.run()
