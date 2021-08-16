b49c4c4a99bec82b22940a76b62d1c8789efb9a5
Matteo <877485@stud.unive.it>
2021-08-16 12:21:05 +0200

    Added error handler 405
    When trying to access a page with the erroneous method (get, post), now there is an error page

--- app/main/errors.py
+++ app/main/errors.py
@@ -14,6 +14,11 @@ def page_not_found(e):
     return render_template("error.html", number=404, message='Not Found'), 400
 
 
+@main.app_errorhandler(405)
+def method_not_allowed(e):
+    return render_template('error.html', number=405, message='Method not allowed'), 405
+
+
 @main.app_errorhandler(500)
 def internal_server_error(e):
     return render_template("error.html", number=500, message='Internal Server Error'), 500
