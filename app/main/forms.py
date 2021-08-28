d247cd7043eb6917724f167801bfd3844922ed9e
Matteo <877485@stud.unive.it>
2021-08-28 20:30:45 +0200

    Changes for display of activable questions
    Effective javascript is still not working, we must think how to set field attributes and change them with js

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
-    SubmitField
-from wtforms.validators import DataRequired, Length, Email, Regexp
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 
 class EditProfileForm(FlaskForm):
